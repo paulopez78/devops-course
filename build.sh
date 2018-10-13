dbb4a74c5c9ef6b6c60a586cd5603de922dd3ef9
paulopez78 <pau.lopez.78@gmail.com>
2018-10-13 18:19:25 +0200

    add docker support for voting api

---
+++ build.sh
@@ -0,0 +1,3 @@
+image="voting-api"
+docker build -t $image -f ./src/$image/Dockerfile ./src/$image
+docker run -p 8081:8081 $image
---
+++ src/voting-api/deps
@@ -0,0 +1,3 @@
+go get github.com/labstack/echo
+go get github.com/labstack/echo/middleware
+go get github.com/gorilla/websocket
