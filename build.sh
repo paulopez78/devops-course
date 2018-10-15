de0c503a6385217d960d24620359cb9701c5aba9
paulopez78 <pau.lopez.78@gmail.com>
2018-10-15 23:52:48 +0200

    build script mimics docker compose

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
 set -e
-image="voting-api"
-docker build -t $image -f ./src/$image/Dockerfile ./src/$image
-docker run -p 8081:8081 $image
+images="voting-api voting-ui"
+port=8081
+
+for image in $images
+do
+  docker build -t $image ./src/$image
+  docker rm -f $image
+  docker run --name $image -d -p $port:80 $image 
+  port=$((port + 1))
+done
